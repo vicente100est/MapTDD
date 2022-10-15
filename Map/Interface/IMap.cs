1c1a157b7e10468dce2c53071cc967edf90d8b4d
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-10-14 20:20:30 -0500

    Add UnitTesting with MSTest and Interface Map whit documentation

---
+++ Map/Interface/IMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map.Interface
+{
+    public interface IMap<TKey, TValue>
+    {
+        /// <summary>
+        /// Put a key value into the map, override pre-existing value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Put(TKey key, TValue value);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns>True if object added, false if not</returns>
+        public bool PutIfAbstract(TKey key, TValue value);
+        public TValue GetValue(TKey key);
+        public IEnumerable<TKey> GetKeys();
+    }
+}
